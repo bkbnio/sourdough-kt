9a70b932c57cb5226f67e842069e2a0c52ee0fa8
Ryan Brink <5607577+rgbrizzlehizzle@users.noreply.github.com>
2021-06-29 18:13:15 -0400

    some misc improvements (#12)

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@ import io.gitlab.arturbosch.detekt.extensions.DetektExtension
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 import com.adarshr.gradle.testlogger.TestLoggerExtension
 import com.adarshr.gradle.testlogger.theme.ThemeType
+import org.unbrokendome.gradle.plugins.testsets.dsl.TestSetContainer
 
 plugins {
   id("org.jetbrains.kotlin.jvm") version "1.5.0" apply false
@@ -10,6 +11,7 @@ plugins {
   id("com.adarshr.test-logger") version "3.0.0" apply false
   id("com.github.jakemarsden.git-hooks") version "0.0.2" apply true
   id("io.github.gradle-nexus.publish-plugin") version "1.1.0" apply true
+  id("org.unbroken-dome.test-sets") version "4.0.0" apply true
 }
 
 gitHooks {
@@ -19,8 +21,8 @@ gitHooks {
   ))
 }
 
-allprojects {
-  group = "io.bkbn"
+subprojects {
+  group = "changeme"
   version = run {
     val baseVersion =
       project.findProperty("project.version") ?: error("project.version must be set in gradle.properties")
@@ -39,10 +41,16 @@ allprojects {
   apply(plugin = "org.jetbrains.kotlin.plugin.serialization")
   apply(plugin = "io.gitlab.arturbosch.detekt")
   apply(plugin = "com.adarshr.test-logger")
+  apply(plugin = "org.unbroken-dome.test-sets")
   apply(plugin = "jacoco")
   apply(plugin = "java-library")
+  apply(plugin = "java-test-fixtures")
   apply(plugin = "maven-publish")
   apply(plugin = "idea")
+
+  configure<TestSetContainer> {
+    create("testIntegration")
+  }
 
   tasks.withType<Test>() {
     useJUnitPlatform()
@@ -89,14 +97,46 @@ allprojects {
     repositories {
       maven {
         name = "GithubPackages"
-        url = uri("https://maven.pkg.github.com/bkbnio/fixme")
+        url = uri("https://maven.pkg.github.com/changeme/changeme")
         credentials {
           username = System.getenv("GITHUB_ACTOR")
           password = System.getenv("GITHUB_TOKEN")
         }
       }
     }
+    publications {
+      create<MavenPublication>("changeme") {
+        groupId = project.group.toString()
+        artifactId = project.name.toLowerCase()
+        version = project.version.toString()
+
+        pom {
+          name.set("changeme")
+          description.set("changeme")
+          url.set("https://github.com/changeme/changeme")
+          licenses {
+            license {
+              name.set("MIT License")
+              url.set("https://mit-license.org/")
+            }
+          }
+          developers {
+            developer {
+              id.set("changeme")
+              name.set("changeme")
+              email.set("changeme")
+            }
+          }
+          scm {
+            connection.set("scm:git:git://github.com/changeme/changeme.git")
+            developerConnection.set("scm:git:ssh://github.com/changeme/changeme.git")
+            url.set("https://github.com/changeme/changeme.git")
+          }
+        }
+      }
+    }
   }
+
   configure<JacocoPluginExtension> {
     toolVersion = "0.8.7"
   }
@@ -108,23 +148,5 @@ nexusPublishing {
       nexusUrl.set(uri("https://s01.oss.sonatype.org/service/local/"))
       snapshotRepositoryUrl.set(uri("https://s01.oss.sonatype.org/content/repositories/snapshots/"))
     }
-  }
-}
-
-tasks.register<JacocoReport>("jacocoRootReport") {
-  subprojects {
-    this@subprojects.plugins.withType<JacocoPlugin>().configureEach {
-      this@subprojects.tasks.matching {
-        it.extensions.findByType<JacocoTaskExtension>() != null }
-        .configureEach {
-          sourceSets(this@subprojects.the<SourceSetContainer>().named("main").get())
-          executionData(this)
-        }
-    }
-  }
-
-  reports {
-    xml.isEnabled = true
-    html.isEnabled = true
   }
 }
--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -5,52 +5,7 @@ plugins {
 }
 
 dependencies {
-    implementation(libs.bundles.logging)
-    testImplementation(libs.bundles.test)
-    detektPlugins(libs.detekt.formatting)
-}
-
-publishing {
-  publications {
-    create<MavenPublication>("changeme") {
-      from(components["kotlin"])
-      artifact(tasks.sourcesJar)
-      artifact(tasks.javadocJar)
-
-      groupId = project.group.toString()
-      artifactId = project.name.toLowerCase()
-      version = project.version.toString()
-
-      pom {
-        name.set("changeme")
-        description.set("changeme")
-        url.set("https://github.com/changeme/changeme")
-        licenses {
-          license {
-            name.set("MIT License")
-            url.set("https://mit-license.org/")
-          }
-        }
-        developers {
-          developer {
-            id.set("changeme")
-            name.set("changeme")
-            email.set("changeme")
-          }
-        }
-        scm {
-          connection.set("scm:git:git://github.com/changeme/changeme.git")
-          developerConnection.set("scm:git:ssh://github.com/changeme/changeme.git")
-          url.set("https://github.com/changeme/changeme.git")
-        }
-      }
-    }
-  }
-}
-
-signing {
-  val signingKey: String? by project
-  val signingPassword: String? by project
-  useInMemoryPgpKeys(signingKey, signingPassword)
-  sign(publishing.publications)
+  implementation(libs.bundles.logging)
+  testImplementation(libs.bundles.test)
+  detektPlugins(libs.detekt.formatting)
 }
