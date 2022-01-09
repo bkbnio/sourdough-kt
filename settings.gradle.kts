999b8fa3fdbcb694a6c762939c636511681e6e60
Ryan Brink <rbweb@pm.me>
2022-01-08 19:22:21 -0500

    feat: killed the catalog

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -7,15 +7,3 @@ include("cli")
 
 // Feature Previews
 enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
-
-// Version Catalog
-object Groups {
-  const val detekt = "io.gitlab.arturbosch.detekt"
-  const val kotest = "io.kotest"
-  const val kotlin = "org.jetbrains.kotlin"
-  const val kotlinx = "org.jetbrains.kotlinx"
-  const val ktor = "io.ktor"
-  const val logback = "ch.qos.logback"
-  const val mockk = "io.mockk"
-  const val slf4j = "org.slf4j"
-}
