e290c306e3c6919b8d6ea670f15f0dcd38c956c1
Ryan Brink <5607577+unredundant@users.noreply.github.com>
2022-08-27 19:36:36 -0600

    better

--- domain/build.gradle.kts
+++ domain/build.gradle.kts
@@ -3,12 +3,25 @@ import kotlinx.kover.api.DefaultJacocoEngine
 plugins {
   kotlin("jvm")
   kotlin("plugin.serialization")
+  id("io.bkbn.sourdough.library.jvm")
   id("io.gitlab.arturbosch.detekt")
   id("com.adarshr.test-logger")
-  id("org.jetbrains.kotlinx.kover")
   id("org.jetbrains.dokka")
+  id("org.jetbrains.kotlinx.kover")
   id("maven-publish")
   id("java-library")
+}
+
+sourdoughLibrary {
+  githubOrg.set("bkbnio")
+  githubRepo.set("sourdough-kt")
+  libraryName.set("sourdough-domain")
+  libraryDescription.set("Sourdough Domain ❤️")
+  licenseName.set("MIT License")
+  licenseUrl.set("https://mit-license.org")
+  developerId.set("unredundant")
+  developerName.set("Ryan Brink")
+  developerEmail.set("admin@bkbn.io")
 }
 
 testing {
@@ -22,8 +35,4 @@ testing {
       }
     }
   }
-}
-
-kover {
-  engine.set(DefaultJacocoEngine)
 }
